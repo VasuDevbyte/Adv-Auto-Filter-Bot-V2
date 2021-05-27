408bcb888a1ac03a25ac7df6b7dbdb0d98162965
VasuDevbyte <81460710+VasuDevbyte@users.noreply.github.com>
2021-05-27 08:47:08 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -129,8 +129,7 @@ async def auto_filter(bot, update):
       
          await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"Damn Bruh! Unfortunately We Couldn't Find This Movie Make Sure Your Spelling?😅",
-                reply_markup=reply_markup,
+                text=f"Damn Bruh! Unfortunately We Couldn't Find This Movie Make Sure Your Spelling?😅",              
                 parse_mode="html",
                 reply_to_message_id=update.message_id
             )
