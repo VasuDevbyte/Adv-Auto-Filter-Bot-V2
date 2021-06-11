1aa1d7a492156b59611f949a7185fadf3b51ae10
VasuDevbyte <81460710+VasuDevbyte@users.noreply.github.com>
2021-06-11 09:22:43 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -220,7 +220,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"Hey There, We Found This📡.../nRequested By ;- {update.from_user.mention} 😎/n● Searched For :- <code>{query}</code> 🔎/n● Total Results ;- {(len_results)} 💯/n🌴 Powered By :- @Explore_Cinema ⚜",
+                text=f"Hey There, We Found This📡...\nRequested By ;- {update.from_user.mention} 😎\n● Searched For :- <code>{query}</code> 🔎\n● Total Results ;- {(len_results)} 💯\n🌴 Powered By :- @Explore_Cinema ⚜",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
